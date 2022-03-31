f42db1c1e008bcdca908870a758b89c9547aea6b
Shivapranav <shivapranav.ct19@bitsathy.ac.in>
2022-03-31 14:45:56 +0530

    src files added🚀

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Home from './Home'
 function App() {
   return (
     <div className="app">
-      <p>Let's goooo!</p>
+      <p>Mussoorie-Tourism</p>
+      <Home />
     </div>
   );
 }
